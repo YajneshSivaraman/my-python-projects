415f2dc40f655779d24ee94775a5f436a89d4942
YajneshSivaraman <yajneshsivaraman05@gmail.com>
2025-01-10 07:05:32 +0530

    number pattern.py

--- Yajnesh python/number pattern.py
+++ Yajnesh python/number pattern.py
@@ -1,7 +1,7 @@
 n=int(input("Enter a no:"))
 for i in range(n+1):
     for j in range(i+1): #initailize range to 1 if you don't want  the program to execute from 0
-        print(i,end=" ") #print j for type 2 number patter
+        print(i,end=" ") #print j for type 2 number pattern
     print("\n")
 
 '''
